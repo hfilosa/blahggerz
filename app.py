8b9ec1839c1c2f854884a7cde01a4f1e76a5b115
Kathy Wang <k.wang6898@gmail.com>
2015-10-15 11:28:20 -0400

    un-deleted the deletions, sqlite create table

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ s['jerry'] = {'password' : 'lei'}
 s['kathy'] = {'password' : 'wang'}
 s.close()
 
+
+
 @app.route("/index", methods=["GET","POST"])
 @app.route("/", methods=["GET","POST"])
 def index():
@@ -50,6 +52,23 @@ def register():
         else:
             return "bye"
 
+@app.route("/postnew", methods=["GET","POST"])
+def postnew():
+    posts = "posts.sqlite"
+    if request.method=="GET":
+        return render_template("postnew.html")
+    if request.method=="POST":
+        con=sqlite3.connect(posts)
+        c = con.cursor()
+      # make username a universal variable?
+        postButton = request.form['"postButton']
+        uname = ""
+        time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+        msg = request.form['post']
+        if postButton = "post": 
+            c.execute("INSERT INTO posts VALUES(uname, time, msg)")
+            return redirect("/index")
+        
 #Debug is true to get better error messages
 #Run the app. The host COULD be IP address, but normally put it down as 0.0.0.0 so that anyone can use the app.
 if __name__=="__main__":
